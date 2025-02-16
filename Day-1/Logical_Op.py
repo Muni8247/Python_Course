03e57a434e759544008fe73fdce61ba1c3b53317
Munireddy Katigari <munireddyk55@gmail.com>
2025-02-16 22:18:48 +0530

    I have added some Python code files

---
+++ Day-1/Logical_Op.py
@@ -0,0 +1,9 @@
+# Three LOgical OP AND OR NOT
+
+#AND
+a=10
+b=15
+print(a>5 and b<10)
+print(a>5 and b<20)
+print(a>10 and b>15)
+print(a>=10 and b<=15)
---
+++ Day-1/Str_methods2.py
@@ -0,0 +1,9 @@
+Name="Naga Muni Reddy"
+print(Name.find("N"))
+
+print(Name.find("Y"))
+
+print(Name.replace("Reddy", " "))
+print(Name)
+print("Reddy" in Name)
+print("Cheddy" in Name)
