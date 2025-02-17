2144cdcfa2705847ef26b545eb59d062e2e0859f
Munireddy Katigari <munireddyk55@gmail.com>
2025-02-17 21:46:40 +0530

    I have created Day-2 folder

---
+++ Day-2/membership.py
@@ -0,0 +1,11 @@
+name='Naga Muni Reddy'
+print('n' in name)
+print('A' in name)
+print(name.upper())
+print('Y' in name)
+
+#List N
+N=[1,2,3,4,5,67,90]
+print(10 in N)
+print(67 in N)
+print(1 in N)
