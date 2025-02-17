6200a98547041a9a198fb2341699ec2afbb6a829
Munireddy Katigari <munireddyk55@gmail.com>
2025-02-17 22:28:14 +0530

    I have added

--- Day-2/f_strings.py
+++ Day-2/f_strings.py
@@ -4,3 +4,6 @@ height=6.0
 weight="96kg"
 
 print(f"Hello my name is {name} and i am {age} years old and my height is {height} my weight is {weight}")
+
+
+#Hello my name is Naga Muni Reddy and i am 25 years old and my height is 6.0 my weight is 96kg
