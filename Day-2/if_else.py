842ab4e6ecba85f0dcc1bc2f32a1a08a572d4994
Munireddy Katigari <munireddyk55@gmail.com>
2025-02-17 23:03:00 +0530

    I have added

---
+++ Day-2/if_else.py
@@ -0,0 +1,14 @@
+time=8
+if (time >= 8):
+    print("Good Morning!!!")
+else:
+    print("Please Sleep!!!")
+    
+    
+hei=int(input("Enter your height: "))
+
+if(hei>=5.8):
+    print("You are eligible for Military")
+else:
+    print("Please try in next time... TQ")
+    
