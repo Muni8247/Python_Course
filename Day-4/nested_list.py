db7e857907edc5813e081f047e108b867977f7ed
Munireddy Katigari <munireddyk55@gmail.com>
2025-02-23 22:58:54 +0530

    I have added Nested list

---
+++ Day-4/nested_list.py
@@ -0,0 +1,17 @@
+ns=[1,2,3,4,5,6,7]
+print(ns)
+print(type(ns))
+
+#Nested List
+ns1=[1,2,3,4,[5,6,7,8],9,10]
+print(ns1)
+print(type(ns1))
+
+print(ns1[0])
+print(ns1[3])
+print(ns1[4])
+print(ns1[4][0])
+print(ns1[4][3])
+print(len(ns1))
+print(ns1[len(ns1)-1])
+print(ns1[len(ns1)-3])
