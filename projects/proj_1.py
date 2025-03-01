02f12273c7c6a9385ce2b59f9b4ea73d22aa0eef
Munireddy Katigari <munireddyk55@gmail.com>
2025-03-01 14:16:36 +0530

    o Have added some in project

--- projects/proj_1.py
+++ projects/proj_1.py
@@ -2,8 +2,8 @@ import random
 print("Welcome to Rock Paper Sciccors game!!!")
 print("0 for Rock, 1 for Paper & 2 for Sciccors")
 user=int(input("User Choice: "))
-if user>=3 and user<0:
-    print("Enter valid number")
+if user>=3 or user<=0:
+    print("You enter invalid number & you loseee!!!")
 else:
     com=random.randint(0,2)
     print(f"Computer choice: {com}")
