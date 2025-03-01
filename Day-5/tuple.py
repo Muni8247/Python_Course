27626550ef2515967ca145e7c65e915135a3140c
Munireddy Katigari <munireddyk55@gmail.com>
2025-03-01 22:47:50 +0530

    I have added tuple in my course

---
+++ Day-5/tuple.py
@@ -0,0 +1,22 @@
+# tp=(1,2,3,4,"Muni",True)
+# print(tp)
+# print(type(tp))
+
+# print(tp[0])
+# print(tp[3])
+# print(tp[-1])
+
+tup=(1,2,3,4,3,2,1,2,3,4,4,3,2,1)
+tup1=(10,20,30)
+print(tup)
+print(tup.count(1))
+print(tup.count(3))
+
+print(tup.index(1))
+print(tup+tup1)
+print(20 in tup)
+print(20 in tup1)
+print(len(tup))
+print(len(tup1))
+print(tup[::2])
+
