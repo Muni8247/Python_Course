3c8abd65e182ad5e836a3a77e4707f4f4d04f8ec
Katigari Naga Muni Reddy <100259996+Muni8247@users.noreply.github.com>
2025-03-13 23:51:22 +0530

    Add files via upload

---
+++ Day-5/Range.py
@@ -0,0 +1,34 @@
+#Range(Start, Stop, Step_Size)
+a=range(10)
+for i in a:
+    print(i)
+
+
+print("**************************************")
+
+
+
+b=range(2,10)
+for j in b:
+    print(j)
+
+
+print("**************************************")
+c=range(2,15,2)
+for x in c:
+    print(x)
+
+
+print("**************************************")
+
+
+
+d=range(2,20,-3)
+for z in d:
+    print(z)
+
+print("**************************************")
+
+e=range(10,0,-1)
+for y in e:
+    print(y)
---
+++ Day-5/while.py
@@ -0,0 +1,4 @@
+num=1
+while num <= 10:
+    print("Hello Naga Muni Reddy")
+    num+=1
